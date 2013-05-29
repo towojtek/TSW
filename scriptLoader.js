c815835968ef9e21d641658b5431d8277ee47bb7
towojtek <towojtek@o2.pl>
2013-05-29 10:16:47 +0200

    Update scriptLoader.js

--- scriptLoader.js
+++ scriptLoader.js
@@ -6,17 +6,11 @@ function loadScript(path, url, fn){
     script.src = path + url;
 	document.getElementsByTagName("head")[0].appendChild(script);
 
-	// Runs the function fn IF you intend to do something after loading the script
-	// Obviously you can use this to load the scripts one after the other, but we don't need this
+	
 	if (fn !== undefined) {
-		// For IE compatibility
+		
 		script.onreadystatechange = function () {
-			/*	You get either one of these based on version of IE.
-				But you don't get both
-
-				Read "http://www.phpied.com/javascript-include-ready-onload/"
-				and "http://unixpapa.com/js/dyna.html" for more information
-			*/
+		
 			if (script.readyState == "complete") {
 				fn();
 			} else if (script.readyState == "loaded") {
